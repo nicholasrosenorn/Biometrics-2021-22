a88f337ae31fcafc3b1de060436400eca9d5411b
Pankaj <meghanipankaj5@gmail.com>
2022-01-21 11:21:43 -0500

    sorted_instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { IoIosArrowBack } from "react-icons/io";
+import instructions from "./instructions.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const raw_data = instructions.instructions;
+    let sorted_instructions = [...raw_data]
+        .sort((a, b) => a.number - b.number)
+        .map((val) => val.instruction);
+    return (
+        <>
+            <header>
+                <IoIosArrowBack />
+            </header>
+            <main></main>
+        </>
+    );
 }
 
 export default App;
