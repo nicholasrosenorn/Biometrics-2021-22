6a14eb62a9545f1575e34f6d71e302c65af87403
Pankaj <meghanipankaj5@gmail.com>
2022-01-25 23:32:50 -0500

    Created basic prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import React from "react";
-import "./App.css";
-import { IoIosArrowBack } from "react-icons/io";
+import React, { useState } from "react";
+import "./App.scss";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import instructions from "./instructions.json";
 
 function App() {
+    const [instructionNumber, setInstructionNumber] = useState(0);
+
     const raw_data = instructions.instructions;
     let sorted_instructions = [...raw_data]
         .sort((a, b) => a.number - b.number)
         .map((val) => val.instruction);
+
     return (
-        <>
+        <div id="main-screen">
             <header>
-                <IoIosArrowBack />
+                <h3>
+                    <IoIosArrowBack /> Back to experiements
+                </h3>
             </header>
-            <main></main>
-        </>
+            <main id="main-screen-content">
+                <h1>{sorted_instructions[instructionNumber]}</h1>
+            </main>
+            <footer id="main-screen-footer">
+                {instructionNumber > 0 ? (
+                    <IoIosArrowBack
+                        onClick={() => {
+                            setInstructionNumber(instructionNumber - 1);
+                        }}
+                        size={"5em"}
+                    />
+                ) : (
+                    <IoIosArrowBack color="grey" size={"5em"} />
+                )}
+
+                {instructionNumber < sorted_instructions.length - 1 ? (
+                    <IoIosArrowForward
+                        onClick={() => {
+                            setInstructionNumber(instructionNumber + 1);
+                        }}
+                        size={"5em"}
+                    />
+                ) : (
+                    <IoIosArrowForward color="grey" size={"5em"} />
+                )}
+            </footer>
+        </div>
     );
 }
 
