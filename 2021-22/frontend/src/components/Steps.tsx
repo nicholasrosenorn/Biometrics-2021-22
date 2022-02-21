6ff9930cef4c97960025c40a70c73b3bb28c4c65
Rebecca Williams <70283367+rwilliams4746@users.noreply.github.com>
2022-02-21 09:11:08 -0500

    fixed create pg css
    spacing issues

--- 2021-22/frontend/src/components/Steps.tsx
+++ 2021-22/frontend/src/components/Steps.tsx
@@ -11,11 +11,14 @@ function Steps() {
           Preheat the oven to 450 degrees Farenheit.
         </label>
         <button className="btn btn__primary btn__lg">
-          Add Input Prompt
+          Add Recording Prompt
         </button>
       </div>
-      <label>
+      <label className="recording">
         Record [Farenheit] Reading of [Oven Temperature]
+      </label>
+      <label className="recording">
+        Record [Celcius] Reading of [Oven Temperature]
       </label>
     </div>
 
