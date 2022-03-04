e333a4b27d6c0cde93b431bf6440dd02fb7aada5
Pankaj <meghanipankaj5@gmail.com>
2022-03-04 11:16:41 -0500

    Update tables.sql
    Diagram at: https://dbdiagram.io/d/6218fda954f9ad109a20d244
    
    ----------------------------------------------------------------------------
    Table "user" {
      "ID" int [pk, not null]
      "username" varchar(100)
      "userPasswordHash" varchar
      userPasswordSalt varchar
      "firstName" varchar
      "lastName" varchar
      "dateOfBirth" datetime
      "biologicalSex" char
    }
    
    Table BiologicalSex {
      sex char [pk, not null]
    }
    
    Table "BioData" {
      "userID" int [pk, not null]
      "timestamp" datetime [pk, not null]
      "minHeartRate" int
      "maxHeartRate" int
      deltaTimeSeconds int
    }
    
    Table templates {
      ID int [pk]
      title varchar
      createdOn datetime
      modifiedOn datetime
      createdBy int
    }
    
    Table template_instructions {
      ID int [pk, not null]
      projectID int [pk, not null]
      index int 
      instruction varchar
    }
    
    Table "Project" {
      projectID int [pk]
      title varchar
      baseTemplate int
      createdBy int
      createdOn datetime
    }
    
    Table "instructions" {
      instructionNumber int [pk, not null]
      projectID int [pk]
      index int
      "instruction" varchar
    }
    
    
    Table batch {
      ID int [pk]
      projectID int [pk]
      experimenter int 
    }
    
    Ref: user.ID < batch.experimenter 
    
    Table compoundSummary {
      batchID int [pk]
      matID varchar [pk]
      compoundName varchar 
      lotNumber varchar
      restestDate datetime
      weightByWeight float
      mgByUnit float
      targetDispense float
      actual float
    }
    
    Table readings_template {
      projectID int [pk]
      reading_of varchar [pk]
      associated_with_instruction int [pk]
      measurement_unit varchar
    }
    
    Ref: instructions.projectID < readings_template.projectID
    Ref: instructions.instructionNumber < readings_template.associated_with_instruction
    
    Table readings {
      batchID int [pk]
      reading_of varchar [pk]
      associated_with_instruction int [pk]
      reading float 
      measurement_unit varchar
    }
    
    Ref: instructions.instructionNumber < readings.associated_with_instruction
    Ref: readings.batchID > batch.ID
    
    Table Steps {
      batchID int [pk]
      instructionNumber int [pk]
      start_time timestamp
      end_time tempstamp
    }
    
    Ref: Steps.batchID > batch.ID
    Ref: Steps.instructionNumber > instructions.instructionNumber
    
    Ref: user.biologicalSex > BiologicalSex.sex
    Ref: user.ID < templates.createdBy
    Ref: templates.ID < template_instructions.projectID
    Ref: BioData.userID > user.ID
    Ref: Project.baseTemplate > templates.ID
    Ref: user.ID < Project.createdBy
    Ref: instructions.projectID > Project.projectID
    Ref: Project.projectID < batch.projectID
    Ref: compoundSummary.batchID > batch.ID

--- 2021-22/Data Engineers/tables.sql
+++ 2021-22/Data Engineers/tables.sql
@@ -1,22 +1,133 @@
-CREATE DATABASE garminData;
-CREATE TABLE garminData.user(
-    ID int NOT NULL,
-	username varchar(100),
-    userPassword varchar(100),
-    firstName varchar(100),
-    lastName varchar(100),
-	DateOfBirth varchar(10),
-	BiologicalSex varchar(1),
-   PRIMARY KEY (ID)
+CREATE TABLE `user` (
+  `ID` int PRIMARY KEY NOT NULL,
+  `username` varchar(100),
+  `userPasswordHash` varchar(255),
+  `userPasswordSalt` varchar(255),
+  `firstName` varchar(255),
+  `lastName` varchar(255),
+  `dateOfBirth` datetime,
+  `biologicalSex` char
 );
-CREATE TABLE garminData.BioData(
-    ID int NOT NULL PRIMARY KEY,
-    minHeartRate int,
-    maxHeartRate int,
-    restingHeartRate int,
-    lastSevenDaysAvgRestingHeartRate int
+
+CREATE TABLE `BiologicalSex` (
+  `sex` char PRIMARY KEY NOT NULL
 );
-CREATE TABLE garminData.instructions(
-    ID int NOT NULL PRIMARY KEY,
-    instructions varchar(100)
-    );
+
+CREATE TABLE `BioData` (
+  `userID` int NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `minHeartRate` int,
+  `maxHeartRate` int,
+  `deltaTimeSeconds` int,
+  PRIMARY KEY (`userID`, `timestamp`)
+);
+
+CREATE TABLE `templates` (
+  `ID` int PRIMARY KEY,
+  `title` varchar(255),
+  `createdOn` datetime,
+  `modifiedOn` datetime,
+  `createdBy` int
+);
+
+CREATE TABLE `template_instructions` (
+  `ID` int NOT NULL,
+  `projectID` int NOT NULL,
+  `index` int,
+  `instruction` varchar(255),
+  PRIMARY KEY (`ID`, `projectID`)
+);
+
+CREATE TABLE `Project` (
+  `projectID` int PRIMARY KEY,
+  `title` varchar(255),
+  `baseTemplate` int,
+  `createdBy` int,
+  `createdOn` datetime
+);
+
+CREATE TABLE `instructions` (
+  `instructionNumber` int NOT NULL,
+  `projectID` int,
+  `index` int,
+  `instruction` varchar(255),
+  PRIMARY KEY (`instructionNumber`, `projectID`)
+);
+
+CREATE TABLE `batch` (
+  `ID` int,
+  `projectID` int,
+  `experimenter` int,
+  PRIMARY KEY (`ID`, `projectID`)
+);
+
+CREATE TABLE `compoundSummary` (
+  `batchID` int,
+  `matID` varchar(255),
+  `compoundName` varchar(255),
+  `lotNumber` varchar(255),
+  `restestDate` datetime,
+  `weightByWeight` float,
+  `mgByUnit` float,
+  `targetDispense` float,
+  `actual` float,
+  PRIMARY KEY (`batchID`, `matID`)
+);
+
+CREATE TABLE `readings_template` (
+  `projectID` int,
+  `reading_of` varchar(255),
+  `associated_with_instruction` int,
+  `measurement_unit` varchar(255),
+  PRIMARY KEY (`projectID`, `reading_of`, `associated_with_instruction`)
+);
+
+CREATE TABLE `readings` (
+  `batchID` int,
+  `reading_of` varchar(255),
+  `associated_with_instruction` int,
+  `reading` float,
+  `measurement_unit` varchar(255),
+  PRIMARY KEY (`batchID`, `reading_of`, `associated_with_instruction`)
+);
+
+CREATE TABLE `Steps` (
+  `batchID` int,
+  `instructionNumber` int,
+  `start_time` timestamp,
+  `end_time` tempstamp,
+  PRIMARY KEY (`batchID`, `instructionNumber`)
+);
+
+ALTER TABLE `batch` ADD FOREIGN KEY (`experimenter`) REFERENCES `user` (`ID`);
+
+ALTER TABLE `readings_template` ADD FOREIGN KEY (`projectID`) REFERENCES `instructions` (`projectID`);
+
+ALTER TABLE `readings_template` ADD FOREIGN KEY (`associated_with_instruction`) REFERENCES `instructions` (`instructionNumber`);
+
+ALTER TABLE `readings` ADD FOREIGN KEY (`associated_with_instruction`) REFERENCES `instructions` (`instructionNumber`);
+
+ALTER TABLE `readings` ADD FOREIGN KEY (`batchID`) REFERENCES `batch` (`ID`);
+
+ALTER TABLE `Steps` ADD FOREIGN KEY (`batchID`) REFERENCES `batch` (`ID`);
+
+ALTER TABLE `Steps` ADD FOREIGN KEY (`instructionNumber`) REFERENCES `instructions` (`instructionNumber`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`biologicalSex`) REFERENCES `BiologicalSex` (`sex`);
+
+ALTER TABLE `templates` ADD FOREIGN KEY (`createdBy`) REFERENCES `user` (`ID`);
+
+ALTER TABLE `template_instructions` ADD FOREIGN KEY (`projectID`) REFERENCES `templates` (`ID`);
+
+ALTER TABLE `BioData` ADD FOREIGN KEY (`userID`) REFERENCES `user` (`ID`);
+
+ALTER TABLE `Project` ADD FOREIGN KEY (`baseTemplate`) REFERENCES `templates` (`ID`);
+
+ALTER TABLE `Project` ADD FOREIGN KEY (`createdBy`) REFERENCES `user` (`ID`);
+
+ALTER TABLE `instructions` ADD FOREIGN KEY (`projectID`) REFERENCES `Project` (`projectID`);
+
+ALTER TABLE `batch` ADD FOREIGN KEY (`projectID`) REFERENCES `Project` (`projectID`);
+
+ALTER TABLE `compoundSummary` ADD FOREIGN KEY (`batchID`) REFERENCES `batch` (`ID`);
+
